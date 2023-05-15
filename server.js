19ed43bb3ff7f9fe8a89122ce67df6d4a3ee9d3a
ben-manning <bmanning@me.com>
2023-05-15 10:53:44 -0400

    define CRUD routes for dogs

--- server.js
+++ server.js
@@ -3,9 +3,31 @@ const app = express();
 const port = 3000;
 
 
-// Routes
-app.get('/', (req, res) => {
+// CRUD Routes RESTful
+
+// GET all dogs
+app.get('/dogs', (req, res) => {
   res.send('Hello World!');
+});
+
+// POST create a dog
+app.post('/dogs', (req, res) => {
+  res.send('This is the POST route for dogs');
+});
+
+// GET one dog
+app.get('/dogs/:id', (req, res) => {
+  res.send('This is the GET route for one dog.');
+});
+
+// PUT one dog
+app.put('/dogs/:id', (req, res) => {
+  res.send('This is the PUT route for one dog.');
+});
+
+// DELETE one dog
+app.delete('/dogs/:id', (req, res) => {
+  res.send('This is the DELETE route for one dog.');
 });
 
 app.listen(port, () => {
